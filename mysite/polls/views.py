d6a8b96401fa67cb1faf177a18cc1a5d8095b377
mikola-s <suharev.nv@gmail.com>
2019-11-30 13:34:13 +0200

    dj polls/view.py write

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def index():
+    return HttpResponse("Hello, word. You're at the polls index")
