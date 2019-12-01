2d1ae631285e1629ab8f48389b97a54c1cd80fb0
mikola-s <suharev.nv@gmail.com>
2019-12-01 20:21:02 +0200

    dj stage 3 complete

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index')
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    # name вызывеется тегом {% url %} шаблона html
+    # можно заменить в шаблоне переопределив path с именем details
+    # path('/cpecifics/<int:question_id>', views.detail, name='detail'),
+    path('<int:question_id>', views.detail, name='detail'),
+
+    # ex: /polls/5/result/
+    path('<int:question_id>/results/', views.result, name='result'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,53 @@
 from django.shortcuts import render
+from django.http import Http404
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
 
 
 # Create your views here.
 
+# def index(request):
+#
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {'latest_question_list': latest_question_list}
+#     return HttpResponse(template.render(context, request))
+
 def index(request):
-    return HttpResponse("Hello, word. You're at the polls index")
+    """ новый index нваисанный с помощью render"""
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+# def detail(request, question_id):
+#     """ вы видите опрос """
+#     return HttpResponse(f"You're looking at question {question_id}.")
+
+# def detail(request, question_id):
+#     """ вариант без сокращения"""
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', {'question': question})
+
+
+from django.shortcuts import get_object_or_404
+
+
+def detail(request, question_id):
+    """ вариант c сокращением"""
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def result(request, question_id):
+    """ вы видите результат опроса """
+    response = f"You're looking at the results of question {question_id}."
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
