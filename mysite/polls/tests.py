160aa531a1cc5fa977608f2c36545371bc7b4aff
mikola-s <suharev.nv@gmail.com>
2019-12-04 09:43:00 +0200

    dj stage 4 complete

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+import datetime
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
 
 # Create your tests here.
+
+class QuestionModelTest()
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,18 +1,33 @@
 from django.urls import path
 from . import views
 
+# app_name = 'polls'
+# urlpatterns = [
+#     path('', views.IndexView.as_view(), name='index'),
+#     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+#     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+#     path('<int:pk>/vote/', views.vote, name='vote'),
+# ]
+
 app_name = 'polls'
 urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    # name вызывеется тегом {% url %} шаблона html
-    # можно заменить в шаблоне переопределив path с именем details
-    # path('/cpecifics/<int:question_id>', views.detail, name='detail'),
-    path('<int:question_id>/', views.detail, name='detail'),
-
-    # ex: /polls/5/result/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+# до первой половины части 4
+# urlpatterns = [
+#         # ex: /polls/
+#     path('', views.index, name='index'),
+#         # ex: /polls/5/
+#         # name вызывеется тегом {% url %} шаблона html
+#         # можно заменить в шаблоне переопределив path с именем details
+#         # path('/specifics/<int:question_id>', views.detail, name='detail'),
+#     path('<int:question_id>/', views.detail, name='detail'),
+#         # ex: /polls/5/result/
+#     path('<int:question_id>/results/', views.results, name='results'),
+#         # ex: /polls/5/vote/
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+# ]
